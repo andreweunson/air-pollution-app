5878104e460de7951914f28d4f6be67844e94468
Andrew Eunson <131382715+andreweunson@users.noreply.github.com>
2024-08-28 15:33:19 -0700

    Moving AirQuality functionality to main app, will host server-side functionality here instead of in a component

--- air-pollution-app/src/App.tsx
+++ air-pollution-app/src/App.tsx
@@ -1,11 +1,34 @@
-
-import './App.css'
-import AirQuality from './assets/components/AirQuality';
+import { useEffect, useState } from "react";
+import "./App.css";
+import AirQuality from "./assets/components/AirQuality";
 
 function App() {
+  const [airData, setAirData] = useState({ sensor: { "pm2.5": 0 } });
+  const [airQuality, setAirQuality] = useState(-1);
+  const myRequest = new Request(
+    "https://api.purpleair.com/v1/sensors/175509?fields=name%2Cpm2.5",
+    {
+      headers: {
+        "X-API-Key": "your api key here",
+      },
+    }
+  );
+
+  useEffect(() => {
+    if (airData.sensor["pm2.5"] !== 0) {
+      return;
+    }
+    fetch(myRequest)
+      .then((response) => response.json())
+      .then((airData) => setAirData(airData));
+  }, [airData, myRequest, setAirData]);
+
+  setAirQuality(airData.sensor["pm2.5"]);
   return (
-    <div><AirQuality /></div>
-  )
+    <div>
+      <AirQuality />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- air-pollution-app/src/assets/components/AirQuality.tsx
+++ air-pollution-app/src/assets/components/AirQuality.tsx
@@ -1,12 +1,10 @@
-//import { useState, useEffect } from "react";
-
 function AirQuality() {
-    return (
-        <div>
-            <h1>Air Quality</h1>
-            <p>Data would go here</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Air Quality</h1>
+      <p>Good</p>
+    </div>
+  );
 }
 
 export default AirQuality;
